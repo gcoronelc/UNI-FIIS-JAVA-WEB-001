bdf9534f51b3ed54b999ae6b44aaf4b7e7c0588c
Gustavo Coronel <gcoronelc@gmail.com>
2018-08-16 12:59:33 -0500

    Clase 04

--- Clase04/EurekaService/src/pe/egcc/eureka/service/impl/ClienteService.java
+++ Clase04/EurekaService/src/pe/egcc/eureka/service/impl/ClienteService.java
@@ -16,15 +16,18 @@ public class ClienteService extends AbstractService implements ClienteEspec {
   public List<ClienteDto> getClientes(String paterno, String materno, String nombre) {
     List<ClienteDto> lista = null;
     Connection cn = null;
+    paterno = "%" + paterno.toUpperCase() + "%";
+    materno = "%" + materno.toUpperCase() + "%";
+    nombre = "%" + nombre.toUpperCase() + "%";
     try {
       cn = AccesoDB.getConnection();
       String sql = "select chr_cliecodigo, vch_cliepaterno, "
               + "vch_cliematerno, vch_clienombre, chr_cliedni, "
               + "vch_clieciudad, vch_cliedireccion, vch_clietelefono, "
               + "vch_clieemail from cliente "
-              + "where vch_cliepaterno like ? "
-              + "and vch_cliematerno like ? "
-              + "and vch_clienombre like ? ";
+              + "where upper(vch_cliepaterno) like ? "
+              + "and upper(vch_cliematerno) like ? "
+              + "and upper(vch_clienombre) like ? ";
       PreparedStatement pstm = cn.prepareStatement(sql);
       pstm.setString(1, paterno);
       pstm.setString(2, materno);
@@ -51,14 +54,14 @@ public class ClienteService extends AbstractService implements ClienteEspec {
     while(rs.next()){
       ClienteDto dto = new ClienteDto();
       dto.setCodigo(rs.getString("CHR_CLIECODIGO"));
-      dto.setCodigo(rs.getString("VCH_CLIEPATERNO"));
-      dto.setCodigo(rs.getString("VCH_CLIEMATERNO"));
-      dto.setCodigo(rs.getString("VCH_CLIENOMBRE"));
-      dto.setCodigo(rs.getString("CHR_CLIEDNI"));
-      dto.setCodigo(rs.getString("VCH_CLIECIUDAD"));
-      dto.setCodigo(rs.getString("VCH_CLIEDIRECCION"));
-      dto.setCodigo(rs.getString("VCH_CLIETELEFONO"));
-      dto.setCodigo(rs.getString("VCH_CLIEEMAIL"));
+      dto.setPaterno(rs.getString("VCH_CLIEPATERNO"));
+      dto.setMaterno(rs.getString("VCH_CLIEMATERNO"));
+      dto.setNombre(rs.getString("VCH_CLIENOMBRE"));
+      dto.setDni(rs.getString("CHR_CLIEDNI"));
+      dto.setCiudad(rs.getString("VCH_CLIECIUDAD"));
+      dto.setDireccion(rs.getString("VCH_CLIEDIRECCION"));
+      dto.setTelefono(rs.getString("VCH_CLIETELEFONO"));
+      dto.setEmail(rs.getString("VCH_CLIEEMAIL"));
       lista.add(dto);
     }
     return lista;
